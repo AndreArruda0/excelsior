bf41ff306494d750a2d6f3a3d967066a86a2c319
André Galdino Arruda <andre-arruda@outlook.com.br>
2020-07-12 03:38:26 -0300

    Upgrade Market page

--- web/src/pages/Market/index.tsx
+++ web/src/pages/Market/index.tsx
@@ -1,12 +1,100 @@
 import React from 'react';
-import {Header, LogoS} from './styles';
+import {Header, LogoS, Products, Admin} from './styles';
 import Logo from '../../assets/logo2.png';
 
 const Market: React.FC = () => {
     return (
-        <Header>
-            <LogoS src={Logo} alt="Logo" />
-        </Header>
+        <>
+            <Header>
+                <LogoS src={Logo} alt="Logo" />
+                <Admin>Admin</Admin>
+            </Header>
+            <Products>
+                <div className="product">
+                <img src="https://static.netshoes.com.br/produtos/casaco-pau-a-pique-moletom/06/DRH-0060-006/DRH-0060-006_zoom1.jpg" alt="casaco" />
+                    <div className="infos">
+                        
+                        <div className="title">
+                            Casasco
+                        </div>
+                        <div className="price">
+                            R$50,00
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className="product">
+                <img src="https://static.netshoes.com.br/produtos/casaco-pau-a-pique-moletom/06/DRH-0060-006/DRH-0060-006_zoom1.jpg" alt="casaco" />
+                    <div className="infos">
+                        
+                        <div className="title">
+                            Casasco
+                        </div>
+                        <div className="price">
+                            R$50,00
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className="product">
+                <img src="https://static.netshoes.com.br/produtos/casaco-pau-a-pique-moletom/06/DRH-0060-006/DRH-0060-006_zoom1.jpg" alt="casaco" />
+                    <div className="infos">
+                        
+                        <div className="title">
+                            Casasco
+                        </div>
+                        <div className="price">
+                            R$50,00
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className="product">
+                <img src="https://static.netshoes.com.br/produtos/casaco-pau-a-pique-moletom/06/DRH-0060-006/DRH-0060-006_zoom1.jpg" alt="casaco" />
+                    <div className="infos">
+                        
+                        <div className="title">
+                            Casasco
+                        </div>
+                        <div className="price">
+                            R$50,00
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className="product">
+                <img src="https://static.netshoes.com.br/produtos/casaco-pau-a-pique-moletom/06/DRH-0060-006/DRH-0060-006_zoom1.jpg" alt="casaco" />
+                    <div className="infos">
+                        
+                        <div className="title">
+                            Casasco
+                        </div>
+                        <div className="price">
+                            R$50,00
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className="product">
+                <img src="https://static.netshoes.com.br/produtos/casaco-pau-a-pique-moletom/06/DRH-0060-006/DRH-0060-006_zoom1.jpg" alt="casaco" />
+                    <div className="infos">
+                        
+                        <div className="title">
+                            Casasco
+                        </div>
+                        <div className="price">
+                            R$50,00
+                        </div>
+                        
+                    </div>
+                </div>
+            </Products>
+        </>
     )
 }
 
--- web/src/pages/Market/styles.ts
+++ web/src/pages/Market/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {shade} from 'polished';
 
 export const Header = styled.div`
     background: #FF002B;
@@ -12,4 +13,67 @@ export const LogoS = styled.img`
     border-radius:10px;
     box-shadow: 0px 3px 6px #00000029;
     margin-top:150px;
+`;
+
+export const Products = styled.div`
+    display:flex;
+    justify-content:center;
+    margin-top:80px;
+    flex-wrap: wrap;
+    padding:20px;
+
+    .product{
+        width: 372px;
+        height: 310px;
+        background: #FFFFFF;
+        box-shadow: 0px 3px 6px #00000029;
+        border: 1px solid #FF002B;
+        text-align:center;
+        transition:transform 0.2s;
+        margin-left:8px;
+        margin-top:8px;
+
+        &:hover{
+            transform: scale(1.1);
+        }
+
+
+        img{
+            height: 226px;
+        }
+
+        .infos{
+            height: 80px;
+            background: #FF002B 0% 0% no-repeat padding-box;
+            color:#FFFFFF;
+
+            .title{
+                font-size:40px;
+                margin-left:12px;
+            }
+
+            .price{
+                float:right;
+                margin-right:12px;
+                font-size:25px;
+            }
+        }
+    }
+`;
+
+export const Admin = styled.button`
+    position: absolute;
+    right:0;
+    margin-top:280px;
+    margin-right:12px;
+    background:#FF0000;
+    border:0px;
+    color:#FFF;
+    padding:12px;
+    border-radius:10px;
+    transition: background-color 0.2s;
+
+    &:hover{
+        background: ${shade(0.3, '#FF0000')};
+    }
 `;
